8be8d340ea1b00c806f5ee12c1113128c43d8a07
NeonDactyl <treyw05@gmail.com>
2019-08-27 16:42:31 -0500

    gross formatting

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,16 @@
 module MoviesHelper
+
+  def format_gross(movie)
+    suffix = ''
+    value = movie.total_gross
+    if movie.total_gross >= 1000000000
+      suffix = 'B'
+      value = movie.total_gross / 1000000000
+    elsif movie.total_gross >= 1000000
+      suffix = 'M'
+      value = movie.total_gross / 1000000
+    end
+    value = number_to_currency(value)
+    value + suffix
+  end
 end
